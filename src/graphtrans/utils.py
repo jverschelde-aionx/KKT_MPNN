c4bfbcf3543904489d7d77c5ce1f0e7aae84847b
jverschelde-aionx <joachim.verschelde@aionx.be>
2025-09-15 10:58:48 +0200

    fix KKTLoss and other fixes

--- src/graphtrans/utils.py
+++
@@ -1,6 +0,0 @@
-def num_total_parameters(model):
-    return sum(p.numel() for p in model.parameters())
-
-
-def num_trainable_parameters(model):
-    return sum(p.numel() for p in model.parameters() if p.requires_grad)
